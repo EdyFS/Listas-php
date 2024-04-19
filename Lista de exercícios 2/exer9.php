ea8c08ffd8572239b061cef8de1cf897bc050d2c
EdyFS <edy.ferreira.santana@gmail.com>
2024-04-18 22:59:53 -0300

    9

--- Lista de exercícios 2/exer9.php
+++ Lista de exercícios 2/exer9.php
@@ -1,1 +1,16 @@
 <?php
+
+$dia = $_POST['dia'];
+$mes = $_POST['mes'];
+$ano = $_POST['ano'];
+
+$idade = 2024 - $ano;
+echo "A idade é ",$idade,"anos";
+
+$dia_mes = $mes * 30;
+$dia_ano = $ano * 365;
+$dias_vividos = $dia + $dia_mes + $dia_ano;
+echo "Dias vividos: ",$dias_vividos;
+
+$idade_2025 = 2025 - $ano;
+echo "Idade em 2025: ",$idade_2025;
