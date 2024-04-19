4c3bd6ee4473d14c6ac907c6ed39d871c2d7de1c
EdyFS <edy.ferreira.santana@gmail.com>
2024-04-19 12:05:33 -0300

    exercicios

--- Lista de Exercícios 3/exer7.php
+++ Lista de Exercícios 3/exer7.php
@@ -1,9 +1,30 @@
 <?php
 
-if (isset($_POST)){
-    if (isset($_POST['valores'])){
-        $vetor = $_POST['valores'];
-        var_dump($vetor);
+if(isset($_POST['submit'])){
+    $aprovados = array();
+    $reprovados = array();
+    foreach ($_POST['alunos'] as $aluno) {
+      $nome = $aluno['nome'];
+      $nota1 = $aluno['n1'];
+      $nota2 = $aluno['n2'];
+      $media = ($nota1 + $nota2) / 2;
+      $aluno['media'] = $media;
+      if ($media >= 6) {
+        $aprovados[] = $aluno;
+      } else {
+        $reprovados[] = $aluno;
+      }
     }
-}
+    echo "Aprovados: ";
+    foreach ($aprovados as $aluno) {
+      echo "Aluno: ", $aluno['nome'], " - Média: ", $aluno['media'];
+    }
+    
+    echo "Reprovados: ";
+    
+    foreach ($reprovados as $aluno) {
+      echo "Aluno: ", $aluno['nome'];
+    }
+
+  }
 ?>
