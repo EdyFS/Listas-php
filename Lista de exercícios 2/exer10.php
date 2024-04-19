42535ccafa72ad9a5b01c18013545a7f505b13a4
EdyFS <edy.ferreira.santana@gmail.com>
2024-04-18 23:26:44 -0300

    10

--- Lista de exercícios 2/exer10.php
+++ Lista de exercícios 2/exer10.php
@@ -1,1 +1,25 @@
 <?php
+
+$masssa = $_POST['massa'];
+$altura = $_POST['altura'];
+
+$imc = $massa / ($altura * $altura);
+
+if ($imc < 18.5){
+    echo "Baixo peso";
+}
+elseif($imc >= 18.5 && $imc < 24.9){
+    echo "Peso normal";
+}
+elseif($imc >= 24.9 && $imc < 29.9){
+    echo "Sobrepeso";
+}
+elseif($imc >= 29.9 && $imc < 34.9){
+    echo "Obesidade grau I";
+}
+elseif($imc >= 34.9 && $imc < 39.9){
+    echo "Obesidade grau II";
+}
+else{
+    echo "Obesidade grau III";
+}
