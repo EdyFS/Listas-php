55fe0b844bb43a9f993b866759a3d972f4dbb833
EdyFS <edy.ferreira.santana@gmail.com>
2024-05-03 22:00:03 -0300

    Update exer4.php

--- Lista de exercícios 2/exer4.php
+++ Lista de exercícios 2/exer4.php
@@ -4,5 +4,5 @@ $numero = $_POST['numero'];
 
 for ($i = 0; $i <= 10; $i++){
     $resultado = $numero * $i;
-    echo  $numero, "x", $i,  "=", $resultado;
+    echo  $numero, "x", $i,  "=", "$resultado<br>";
 }
