6438b8c83461c8945bc6c02b3d910a4b8fdfc872
EdyFS <edy.ferreira.santana@gmail.com>
2024-05-10 21:49:03 -0300

    Lista

---
+++ Lista 1 com Bootstraps/Exercicio1/exer1.php
@@ -0,0 +1,16 @@
+<?php
+require_once "cabecalho.php";
+?>
+
+<form action="exer1resp.php" method="post">
+<div class="row">
+    <div class="col">
+        <label for="valor1" class="form-label">Informe o número</label>
+        <input type="number" name="valor1" id="valor1" class="form-control">
+    </div>
+    <div class="col">
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </div>
+</div>
+</form>
+
