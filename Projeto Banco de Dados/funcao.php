1d584ddbf147195528b6b7126ad92a0b361c0d1d
EdyFS <edy.ferreira.santana@gmail.com>
2024-05-17 22:15:29 -0300

    Update funcao.php

--- Projeto Banco de Dados/funcao.php
+++ Projeto Banco de Dados/funcao.php
@@ -3,5 +3,15 @@
 function conectarBanco(){
     $conexao = new PDO("mysql:host=localhost; dbname=bancophp", "root","");
     return $conexao;
-    
+
+}
+
+function retornarCategorias(){
+    try{
+        $sql = "SELECT * FROM categoria";
+        $conexao = conectarBanco();
+        return $conexao->query($sql);
+    } catch(Exception $e) {
+        return 0;
+    }
 }
