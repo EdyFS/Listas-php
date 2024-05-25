64efa9f5542ec4aa02d6318458e0deed8109b308
EdyFS <edy.ferreira.santana@gmail.com>
2024-05-25 11:20:56 -0300

    Atualização

--- ProjetoHotel/cabecalho.php
+++ ProjetoHotel/cabecalho.php
@@ -7,6 +7,28 @@
     <title>Document</title>
 </head>
 <body>
-    
-</body>
-</html>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Hotel</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+              <a class="nav-link" href="LIsta-php/Lista-php/Produtos/index.php">Hóspede</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="LIsta-php/Lista-php/Produtos/index.php">Quartos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="LIsta-php/Lista-php/Produtos/index.php">Reservas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="LIsta-php/Lista-php/Produtos/index.php">Serviços</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <main class="container">
---
+++ ProjetoHotel/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once("cabecalho.php");
+?>
+<h1>Sua melhor experiência no melhor conforto</h1>
+<?php
+require_once("rodape.html");
