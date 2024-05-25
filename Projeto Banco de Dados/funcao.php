743995baf0dfda3529f5b8e3dae2ab15aa0b1255
EdyFS <edy.ferreira.santana@gmail.com>
2024-05-24 21:55:48 -0300

    Update funcao.php

--- Projeto Banco de Dados/funcao.php
+++ Projeto Banco de Dados/funcao.php
@@ -17,6 +17,17 @@ function retornarCategorias(){
 
 }
 
+function retornarProdutos(){
+    try {
+        $sql = "SELECT p.*, c.descricao as categoria FROM produto p
+                INNER JOIN categoria c ON c.id = p.categoria_id";
+        $conexao = conectarBanco();
+        return $conexao->query($sql);
+    } catch (Exception $e) {
+        return 0;
+    }
+}
+
 function inserirProduto($nome, $descricao, $valor, $categoria){
         try{
             $sql = "INSERT INTO produto (nome, descricao, valor, categoria) VALUES (:nome, :descricao, :valor, :categoria)";
