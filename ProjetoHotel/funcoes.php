ae85e6d4c3b57f249be2424bc80282ab8ad3f5c9
EdyFS <edy.ferreira.santana@gmail.com>
2024-06-08 10:19:07 -0300

    hotel

---
+++ ProjetoHotel/funcoes.php
@@ -0,0 +1,23 @@
+function inserirHospede
+
+function alterarHospede
+
+function excluirHospede
+
+function inserirReserva
+
+function alterarReserva
+
+function excluirReserva
+
+function inserirQuarto
+
+function alterarQuarto
+
+function excluirQuarto
+
+function inserirServico
+
+function alterarServico
+
+function excluirServico
